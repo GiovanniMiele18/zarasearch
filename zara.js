351ab605e64bc04a8060257de6fe52c322633450
Giovanni Miele <106318586+GiovanniMiele18@users.noreply.github.com>
2025-02-13 18:35:01 +0100

    Update zara.js

--- zara.js
+++ zara.js
@@ -1,7 +1,9 @@
-fetch("https://songgenerated.altervista.org/zara.php")
+fetch("https://songgenerated.altervista.org/zara.php") // Sostituisci con il tuo URL AlterVista
     .then(res => res.json())
     .then(data => {
-        console.log("Dati ricevuti:", data);
-        // Estrarre le taglie dal JSON ricevuto
+        console.log("Taglie disponibili:", data.sizes);
+
+        // Stampa le taglie nella pagina
+        document.getElementById("taglie").innerHTML = data.sizes.join(", ");
     })
     .catch(err => console.error("Errore:", err));
